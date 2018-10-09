bf5ed03e62cca5f37db0d878c3af3ec1fb0e8e69
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-09 09:07:09 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,9 @@
 class Shoe
-  attr_accessor :condition, :size, :material, :color
+  attr_accessor :condition, :size, :material, :color, :brand
   def initialize(brand)
     @brand = brand
   end
+  def cobble
+    puts 
+  end 
 end
