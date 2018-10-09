143670f9490473570e312a77cf98b916989b9eb0
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-09 09:00:33 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,13 @@
 
 
 class Book
+
   attr_accessor :genre, :page_count, :author
+
   def initialize(title)
     @title = title
   end
+
   def turn_page
     puts "Turning the page... Wow, you read fast!"
   end
